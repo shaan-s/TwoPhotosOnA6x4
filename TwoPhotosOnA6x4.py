a1c2ca17ecbfb76cd761b60596f5b6de97b55034
Mcguy215 <31831945+Mcguy215@users.noreply.github.com>
2023-04-21 01:04:13 -0400

    Update TwoPhotosOnA6x4.py

--- TwoPhotosOnA6x4.py
+++ TwoPhotosOnA6x4.py
@@ -6,6 +6,11 @@ import sys
 from PIL import Image
 from time import time
 
+finalw=1800
+finalh=1200
+#6in x 4in at 300dpi
+margin=100
+
 if len(sys.argv) < 3:
     print("ERROR: You must drag + drop EXACTLY TWO files. Exiting now...")
     errExit()
@@ -17,14 +22,14 @@ print("Images imported... Working...")
 for x in range(2):
     if imgs[x].width > imgs[x].height:
         imgs[x]=imgs[x].rotate(90,expand=1)
-    imgs[x]=imgs[x].resize((int(2200*(imgs[x].width/imgs[x].height)),2200))
-    if imgs[x].width > 1800:
-        imgs[x]=imgs[x].resize((1600,(int(1800*(imgs[x].height/imgs[x].width)))))
+    imgs[x]=imgs[x].resize((int((finalh-margin)*(imgs[x].width/imgs[x].height)),(finalh-margin)))
+    if imgs[x].width > finalw//2:
+        imgs[x]=imgs[x].resize((finalw//2-margin,(int((finalw//2-margin)*(imgs[x].height/imgs[x].width)))))
 
-im = Image.new("RGB", (3600, 2400), (255,255,255)) #6in x 4in at 600dpi
+im = Image.new("RGB", (finalw, finalh), (255,255,255))
 
-im.paste(imgs[0],((1800-imgs[0].width)//2,100))
-im.paste(imgs[1],(1800+(1800-imgs[1].width)//2,100))
+im.paste(imgs[0],((finalw//2-imgs[0].width)//2,margin//2))
+im.paste(imgs[1],(finalw//2+(finalw//2-imgs[1].width)//2,margin//2))
 
 filename=f"{round(time())}.jpg"
 im.save(filename, format=None)
